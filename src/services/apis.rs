da7bb8809748c6c2d4a8da2341557b63b06efa34
JoaoPedroPP <jp052@hotmail.com>
2023-02-10 15:51:16 -0300

    :construction:

--- src/services/apis.rs
+++ src/services/apis.rs
@@ -1,5 +1,10 @@
+use std::env;
 use actix_multipart::Multipart;
 use futures_util::StreamExt;
+use rdkafka::{
+    producer::{BaseProducer, BaseRecord},
+    ClientConfig,
+};
 use actix_web::{
     HttpResponse,
     Error,
@@ -17,4 +22,31 @@ pub async fn upload_cos(mut payload: Multipart) -> Result<HttpResponse, Error> {
         // let filepath = format!("./tmp/{filename}");
     }
     Ok(HttpResponse::Ok().body("ssss"))
+}
+
+pub async fn trigger_kafka() -> Result<HttpResponse, Error> {
+    let kafka_servers = env::var("KAFKA_SERVERS").expect("KAFKA_SERVERS not set");
+    let kafka_username = env::var("KAFKA_USERNAME").expect("KAFKA_USERNAME not set");
+    let kafka_password = env::var("KAFKA_PASSWORD").expect("KAFKA_PASSWORD not set");
+    let kafka_producer_topic = env::var("KAFKA_PRODUCER_TOPIC").expect("KAFKA_PRODUCER_TOPIC not set");
+
+    let producer: BaseProducer = ClientConfig::new()
+        .set("bootstrap.servers", kafka_servers)
+        .set("sasl.username", kafka_username)
+        .set("sasl.password", kafka_password)
+        .set("security.protocol", "SASL_SSL")
+        .set("sasl.mechanisms", "PLAIN")
+        .create()
+        .expect("Invalid Kafka Producer Config");
+
+    match producer.send(BaseRecord::to(&kafka_producer_topic).key("msg").payload("data")) {
+        Ok(_) => {
+            println!("Submited to Kafka");
+            Ok(HttpResponse::Ok().body("uuuu"))
+        },
+        Err(_) => {
+            println!("Msg could not be sent to kafka");
+            Ok(HttpResponse::InternalServerError().body("Error"))
+        }
+    }
 }
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,3 +1,4 @@
 mod apis;
 
 pub use apis::upload_cos;
+pub use apis::trigger_kafka;
